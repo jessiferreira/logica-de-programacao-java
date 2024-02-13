5bde9fa030a17411ab0a3837624d7a04038e31ab
Jéssica <ferrsjessy@gmail.com>
2024-02-13 08:19:01 -0300

    Processamento de dados / casting

---
+++ src/Aula2.java
@@ -0,0 +1,77 @@
+public class Aula2 {
+
+
+/*
+    public static void main(String[] args) {
+
+        int x, y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+_______________________________________________
+
+    public static void main(String[] args) {
+
+        int x;
+        double y;
+
+        x = 5;
+        y = 2 * x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+_______________________________________________
+
+    public static void main(String[] args) {
+
+        double b, B, h, area;
+
+        b = 6.0;
+        B = 8.0;
+        h = 5.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+
+    }
+}
+_______________________________________________
+
+public static void main(String[] args) {
+
+        int a, b;
+        double resultado;
+
+        a = 5;
+        b = 2;
+
+        resultado = (double) a / b; // Casting
+
+        System.out.println(resultado);
+
+    }
+}
+
+*/
+
+    public static void main(String[] args) {
+
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a; // Casting
+
+        System.out.println(b);
+
+    }
+}
+
