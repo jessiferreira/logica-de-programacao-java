89b8878c2758e491669ae9272cc61ae1827fb080
Jéssica <ferrsjessy@gmail.com>
2024-02-16 15:52:41 -0300

    Sintaxe Opcional: Operadores de Atribuição Cumulativa

--- src/Aula7.java
+++ src/Aula7.java
@@ -6,11 +6,9 @@ import java.util.Scanner;
 public class Aula7 {
 
 /*
-* PROBLEMA EXEMPLO: Uma operadora de telefonia cobra R$ 50.00 por um plano básico que
-dá direito a 100 minutos de telefone. Cada minuto que exceder a
-franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a
-quantidade de minutos que uma pessoa consumiu, daí mostrar o valor
-a ser pago.
+* PROBLEMA EXEMPLO: Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de
+telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a quantidade
+de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
 */
 
     public static void main(String[] args) {
---
+++ src/Aula8.java
@@ -0,0 +1,11 @@
+public class Aula8 {
+
+/*
+Fazer um programa para ler um valor inteiro de 1 a 7 representando um dia da semana (sendo 1 = domingo,
+2 = segunda, e assim por diante). Escrever na tela o dia da semana correspondente.
+*/
+
+    public static void main(String[] args) {
+
+    }
+}
